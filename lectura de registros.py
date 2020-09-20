d3f31c4151a721f19501e9b8d5d05299e0d2195a
yulienis <yulienis.munoz@uic.cu>
2020-09-20 03:03:05 -0400

    Tarea del dia completada...seguimos aprendiendo

---
+++ lectura de registros.py
@@ -0,0 +1,6 @@
+agenda = open("/home/yulienis/DEV/agendatelefonica.txt")
+#Para leer imprimir toda la agenda
+#print(agenda.read())
+
+#Una opcion para leer y mostrar de forma selectiva
+print(agenda.readline())
